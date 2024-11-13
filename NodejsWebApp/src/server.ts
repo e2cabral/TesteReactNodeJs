fdb26cd7ea964599f5839771defe07335b2687e9
Edson Cabral Junior <e2cabraljunior@gmail.com>
2024-11-13 09:00:10 -0300

    chore: enable CORS in the server configuration
    Added CORS middleware to allow cross-origin requests. Reordered the configureRoutes function to be called after middleware setup to ensure proper middleware usage.

--- NodejsWebApp/src/server.ts
+++ NodejsWebApp/src/server.ts
@@ -1,16 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const configureRoutes = require('./main/config/routes.config');
 const app = express();
 const router = express.Router();
 
+app.use(cors());
+app.use(bodyParser.json());
 configureRoutes(app, router)
 
-app.use(bodyParser.json());
-
-
-
-const port = 3000; /* process.env.PORT || */
+const port = 3000;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port} - v.0.11`);
